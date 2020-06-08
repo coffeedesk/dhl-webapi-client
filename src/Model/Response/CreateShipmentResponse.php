a6bdb9e852027247fd26ac59bb04b87cfcf6e60a
lukaszgrot <lugrot@gmail.com>
2020-06-08 14:38:12 +0200

    Fix problem with overwriting class (CreateShipmentResponse)

---
+++ src/Model/Response/CreateShipmentResponse.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace DHLClient\Model\Response;
+
+
+use DHLClient\Model\Label;
+
+class CreateShipmentResponse
+{
+    /** @var string */
+    protected $shipmentNotificationNumber;
+
+    /** @var string */
+    protected $shipmentTrackingNumber;
+
+    /** @var string|null */
+    protected $packagesTrackingNumbers;
+
+    /** @var string|null */
+    protected $dispatchNotificationNumber;
+
+    /** @var Label */
+    protected $label;
+
+    /**
+     * @return string
+     */
+    public function getShipmentNotificationNumber()
+    {
+        return $this->shipmentNotificationNumber;
+    }
+
+    /**
+     * @param string $shipmentNotificationNumber
+     */
+    public function setShipmentNotificationNumber($shipmentNotificationNumber)
+    {
+        $this->shipmentNotificationNumber = $shipmentNotificationNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShipmentTrackingNumber()
+    {
+        return $this->shipmentTrackingNumber;
+    }
+
+    /**
+     * @param string $shipmentTrackingNumber
+     */
+    public function setShipmentTrackingNumber($shipmentTrackingNumber)
+    {
+        $this->shipmentTrackingNumber = $shipmentTrackingNumber;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPackagesTrackingNumbers()
+    {
+        return $this->packagesTrackingNumbers;
+    }
+
+    /**
+     * @param string|null $packagesTrackingNumbers
+     */
+    public function setPackagesTrackingNumbers($packagesTrackingNumbers)
+    {
+        $this->packagesTrackingNumbers = $packagesTrackingNumbers;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDispatchNotificationNumber()
+    {
+        return $this->dispatchNotificationNumber;
+    }
+
+    /**
+     * @param string|null $dispatchNotificationNumber
+     */
+    public function setDispatchNotificationNumber($dispatchNotificationNumber)
+    {
+        $this->dispatchNotificationNumber = $dispatchNotificationNumber;
+    }
+
+    /**
+     * @return Label
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param Label $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+}
